1715f3c137e0ba5fe7688997b019d3bdab5a1c95
rsqjpn <jpanakiki@gmail.com>
2025-02-06 13:31:52 +0700

    test

---
+++ resources/views/DashboardAdmin/achievement/piagam/index.blade.php
@@ -0,0 +1,12 @@
+@extends('DashboardAdmin.layout')
+
+@section('title', 'Daftar piagam')
+
+@section('content')
+    <div class="sm:ml-64 p-6">
+        <h1>test</h1>
+    </div>
+
+
+
+    @endsection
