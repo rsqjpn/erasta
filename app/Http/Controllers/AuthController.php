8aa0ebf63ba40d3e0be710f8eb86700bdc9f007a
rsqjpn <jpanakiki@gmail.com>
2025-02-09 14:08:06 +0700

    ----

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Users;
+use App\Models\users;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
